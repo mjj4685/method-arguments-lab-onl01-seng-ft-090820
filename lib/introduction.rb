b891cd59f7b45d1e1bdc82b15abf8a3d881eb306
MJ Johnson <mjohnson4685@gmail.com>
2020-09-09 16:23:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,3 @@ end
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
-def introduction_with_language(name,language)
-  puts "Hi, my name is #{Dan} and I am learning to program in #{Ember.js}."
-end
