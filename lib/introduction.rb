5936739d1ad0951682e6e582516ac33209feb213
MJ Johnson <mjohnson4685@gmail.com>
2020-09-09 16:22:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,6 +8,7 @@ def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language("Dan", "Ember.js")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{Dan} and I am learning to program in #{Ember.js}."
 end
+
