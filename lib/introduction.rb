a3db5365a42ffc4cb6f2a521ad6516d08f1768bb
MJ Johnson <mjohnson4685@gmail.com>
2020-09-09 16:10:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,9 @@
 
 
-def introduction(name,language)
+def introduction(name, language)
   puts "Hi, my name is #{name}. I am a great #{language} programmer."
 end
+
+introduction("Sophie", "Ruby")
+
+  
